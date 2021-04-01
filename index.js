269f1222760e130cb132f8d4a83c18655fd749a4
Benoît Guérout <bguerout@gmail.com>
2021-04-01 12:01:35 +0200

    Add chunk

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ module.exports = {
   transformData,
   transformIntoJSON: require("./lib/transformIntoJSON.js"),
   transformIntoCSV: require("./lib/transformIntoCSV.js"),
-  readLineByLine: require("./lib/readLineByLine"),
-  flattenArray: require("./lib/flattenArray.js"),
   filterData: (filter) => transformData((data) => data, { filter }),
   accumulateData: require("./lib/accumulateData.js"),
+  chunk: require("./lib/chunk"),
+  readLineByLine: require("./lib/readLineByLine"),
+  flattenArray: require("./lib/flattenArray.js"),
   writeData: require("./lib/writeData.js"),
   writeToStdout: require("./lib/writeToStdout.js"),
 };
