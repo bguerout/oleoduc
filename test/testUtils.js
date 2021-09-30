28ce65be817cd0677d1ecc3b4bd3c487bd09588c
Benoit Guérout <bguerout@gmail.com>
2021-09-30 14:01:01 +0200

    Allow streams to be merged sequentially (#35)

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,9 +1,23 @@
+const { Readable } = require("stream");
+
+function delay(callback, delay) {
+  return new Promise((resolve) => {
+    return setTimeout(async () => {
+      resolve(callback());
+    }, delay);
+  });
+}
+
+function createStream(items) {
+  return new Readable({
+    objectMode: true,
+    read() {
+      this.push(items.length === 0 ? null : items.pop());
+    },
+  });
+}
+
 module.exports = {
-  delay: (callback, delay) => {
-    return new Promise((resolve) => {
-      return setTimeout(async () => {
-        resolve(callback());
-      }, delay);
-    });
-  },
+  createStream,
+  delay,
 };
