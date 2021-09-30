c578472b90f7e588943c83c1d4d84b4eca0d349e
Benoît Guérout <bguerout@gmail.com>
2021-09-30 08:48:55 +0200

    Allow streams to be merged sequentially

--- test/mergeStreams-test.js
+++ test/mergeStreams-test.js
@@ -1,24 +1,12 @@
 const assert = require("assert");
-const { Readable } = require("stream");
 const { mergeStreams, writeData } = require("../index");
-
-const createStream = () => {
-  return new Readable({
-    objectMode: true,
-    read() {},
-  });
-};
+const { createStream } = require("./testUtils");
 
 describe("mergeStreams", () => {
   it("can merge multiple streams", (done) => {
     let result = "";
-    let source1 = createStream();
-    source1.push("andré");
-    source1.push(null);
-
-    let source2 = createStream();
-    source2.push("bruno");
-    source2.push(null);
+    let source1 = createStream(["andré"]);
+    let source2 = createStream(["bruno"]);
 
     mergeStreams(source1, source2)
       .pipe(writeData((data) => (result += data)))
@@ -28,14 +16,62 @@ describe("mergeStreams", () => {
       });
   });
 
-  it("can iterate over a merged stream", async () => {
-    let source1 = createStream();
-    source1.push("andré");
-    source1.push(null);
+  it("can merge multiple streams (factory)", (done) => {
+    let result = "";
+    mergeStreams(
+      () => createStream(["andré"]),
+      () => createStream(["bruno"])
+    )
+      .pipe(writeData((data) => (result += data)))
+      .on("finish", () => {
+        assert.deepStrictEqual(result, "andrébruno");
+        done();
+      });
+  });
 
-    let source2 = createStream();
-    source2.push("bruno");
-    source2.push(null);
+  it("can merge multiple streams (async factory)", (done) => {
+    let result = "";
+    mergeStreams(
+      () => Promise.resolve(createStream(["andré"])),
+      () => Promise.resolve(createStream(["bruno"]))
+    )
+      .pipe(writeData((data) => (result += data)))
+      .on("finish", () => {
+        assert.deepStrictEqual(result, "andrébruno");
+        done();
+      });
+  });
+
+  it("can merge multiple streams (sequentially)", (done) => {
+    let result = "";
+    let source1 = createStream(["andré"]);
+    let source2 = createStream(["bruno"]);
+
+    mergeStreams(source1, source2, { sequential: true })
+      .pipe(writeData((data) => (result += data)))
+      .on("finish", () => {
+        assert.deepStrictEqual(result, "andrébruno");
+        done();
+      });
+  });
+
+  it("can merge multiple streams (sequentially+factory)", (done) => {
+    let result = "";
+    mergeStreams(
+      () => createStream(["andré"]),
+      () => createStream(["bruno"]),
+      { sequential: true }
+    )
+      .pipe(writeData((data) => (result += data)))
+      .on("finish", () => {
+        assert.deepStrictEqual(result, "andrébruno");
+        done();
+      });
+  });
+
+  it("can iterate over a merged stream", async () => {
+    let source1 = createStream(["andré"]);
+    let source2 = createStream(["bruno"]);
 
     let chunks = [];
     for await (const chunk of mergeStreams(source1, source2)) {
