4d9240a894e8cbe2f25bb0f9dd73af0d3dfd15aa
Benoit Guérout <bguerout@gmail.com>
2022-08-30 14:07:59 +0200

    Remove sequential option from mergeStreams (#46)

--- test/mergeStreams-test.js
+++ test/mergeStreams-test.js
@@ -3,65 +3,12 @@ const { mergeStreams, writeData } = require("../index");
 const { createStream } = require("./testUtils");
 
 describe("mergeStreams", () => {
-  it("can merge multiple streams", (done) => {
+  it("can merge streams", (done) => {
     let result = "";
     const source1 = createStream(["andré"]);
     const source2 = createStream(["bruno"]);
 
     mergeStreams(source1, source2)
-      .pipe(writeData((data) => (result += data)))
-      .on("finish", () => {
-        assert.deepStrictEqual(result, "andrébruno");
-        done();
-      });
-  });
-
-  it("can merge multiple streams (factory)", (done) => {
-    let result = "";
-    mergeStreams(
-      () => createStream(["andré"]),
-      () => createStream(["bruno"])
-    )
-      .pipe(writeData((data) => (result += data)))
-      .on("finish", () => {
-        assert.deepStrictEqual(result, "andrébruno");
-        done();
-      });
-  });
-
-  it("can merge multiple streams (async factory)", (done) => {
-    let result = "";
-    mergeStreams(
-      () => Promise.resolve(createStream(["andré"])),
-      () => Promise.resolve(createStream(["bruno"]))
-    )
-      .pipe(writeData((data) => (result += data)))
-      .on("finish", () => {
-        assert.deepStrictEqual(result, "andrébruno");
-        done();
-      });
-  });
-
-  it("can merge multiple streams (sequentially)", (done) => {
-    let result = "";
-    const source1 = createStream(["andré"]);
-    const source2 = createStream(["bruno"]);
-
-    mergeStreams(source1, source2, { sequential: true })
-      .pipe(writeData((data) => (result += data)))
-      .on("finish", () => {
-        assert.deepStrictEqual(result, "andrébruno");
-        done();
-      });
-  });
-
-  it("can merge multiple streams (sequentially+factory)", (done) => {
-    let result = "";
-    mergeStreams(
-      () => createStream(["andré"]),
-      () => createStream(["bruno"]),
-      { sequential: true }
-    )
       .pipe(writeData((data) => (result += data)))
       .on("finish", () => {
         assert.deepStrictEqual(result, "andrébruno");
