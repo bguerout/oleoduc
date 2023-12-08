2a0159d2239f917f441e8a6b3585fa0fdc74c624
Benoît Guérout <bguerout@gmail.com>
2023-12-08 22:00:05 +0100

    Build project to test against nodejs versions

--- .tools/build.sh
+++ .tools/build.sh
@@ -2,26 +2,36 @@
 set -euo pipefail
 
 readonly PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/.."
+readonly BUILD_DIR="${PROJECT_DIR}/build"
+readonly DIST_DIR="${PROJECT_DIR}/dist"
 
-rm -rf "${PROJECT_DIR}/dist"
+rm -rf "${DIST_DIR}" "${BUILD_DIR}"
+mkdir -p "${DIST_DIR}" "${BUILD_DIR}"
 
 cd "${PROJECT_DIR}"
 
-echo "Building oleoduc (esm version)..."
+echo "Compiling oleoduc (esm version)..."
 tsc -p tsconfig.json
-cat >"./dist/mjs/package.json" <<!EOF
+cat >"${DIST_DIR}/mjs/package.json" <<!EOF
 {
     "type": "module"
 }
 !EOF
 
-echo "Building oleoduc (cjs version)..."
+echo "Compiling oleoduc (cjs version)..."
 tsc -p tsconfig.cjs.json
-cat >"./dist/cjs/package.json" <<!EOF
+cat >"${DIST_DIR}/cjs/package.json" <<!EOF
 {
     "type": "commonjs"
 }
 !EOF
+
+echo "Building oleoduc for test..."
+tsc -p tsconfig.test.json
+echo "Patching package.json to be able to run tests against previous versions of nodejs..."
+cp "${PROJECT_DIR}/package.json" "${BUILD_DIR}"
+npx json -I -f "${BUILD_DIR}/package.json" -e 'this.scripts.test="mocha --recursive --exit test/**/*-test.js"'
+npx json -I -f "${BUILD_DIR}/package.json" -e 'this.devDependencies.mocha="9.x"'
 
 cd -
 
