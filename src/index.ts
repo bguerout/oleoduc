166beab5687042ea685b4eed9b966ebb4d4d5623
Benoît Guérout <bguerout@gmail.com>
2025-01-06 19:33:45 +0100

    export TS types

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,28 @@
-import {accumulateData} from "./accumulateData.ts";
-import {pipeStreams} from "./pipeStreams.ts";
+import {accumulateData, AccumulateDataCallback, AccumulateDataOptions} from "./accumulateData.ts";
+import {pipeStreams, PipeStreamsOptions} from "./pipeStreams.ts";
 import {concatStreams} from "./concatStreams.ts";
 import {filterData} from "./filterData.ts";
 import {flattenArray} from "./flattenArray.ts";
-import {groupData} from "./groupData.ts";
+import {groupData, GroupDataOptions} from "./groupData.ts";
 import {mergeStreams} from "./mergeStreams.ts";
-import {oleoduc} from "./oleoduc.ts";
+import {oleoduc, OleoducOptions} from "./oleoduc.ts";
 import {readLineByLine} from "./readLineByLine.ts";
-import {transformData} from "./transformData.ts";
-import {transformIntoCSV} from "./transformIntoCSV.ts";
-import {transformIntoJSON} from "./transformIntoJSON.ts";
-import {transformStream} from "./transformStream.ts";
-import {writeData} from "./writeData.ts";
+import {transformData, TransformDataCallback, TransformDataOptions} from "./transformData.ts";
+import {transformIntoCSV, TransformIntoCSVOptions} from "./transformIntoCSV.ts";
+import {transformIntoJSON, TransformIntoJSONOptions} from "./transformIntoJSON.ts";
+import {transformStream, TransformStreamCallback} from "./transformStream.ts";
+import {writeData, WriteDataCallback, WriteDataOptions} from "./writeData.ts";
 import {writeToStdout} from "./writeToStdout.ts";
-
-//Legacy from 0.8.x
-const compose = pipeStreams;
 
 export {
     accumulateData,
-    pipeStreams,
-    compose,
     concatStreams,
     filterData,
     flattenArray,
     groupData,
     mergeStreams,
     oleoduc,
+    pipeStreams,
     readLineByLine,
     transformData,
     transformIntoCSV,
@@ -34,4 +30,23 @@ export {
     transformStream,
     writeData,
     writeToStdout,
+};
+
+//Legacy from 0.8.x
+export const compose = pipeStreams;
+
+//Types
+export {
+    AccumulateDataCallback,
+    AccumulateDataOptions,
+    GroupDataOptions,
+    OleoducOptions,
+    PipeStreamsOptions,
+    TransformDataCallback,
+    TransformDataOptions,
+    TransformIntoCSVOptions,
+    TransformIntoJSONOptions,
+    TransformStreamCallback,
+    WriteDataCallback,
+    WriteDataOptions,
 };
